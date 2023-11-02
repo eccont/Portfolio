3b56a1a2a0d7d202c34a61df3d12f2ec93afb1ac
Erick Contrera <erickc.contrera@gmail.com>
2023-11-02 17:04:50 -0300

    atts

--- SCRIPT/dark-mode.js
+++ SCRIPT/dark-mode.js
@@ -4,14 +4,9 @@ let changeImg = document.getElementById("mudar-foto");
 button.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 
-  caminho = location.href.replace('index.html', '');
+  caminho = location.href.replace("index.html", "");
   var img1 = caminho + "ASSETS/minha-carinha2.jpg";
-  var img2 = caminho + "ASSETS/erick-light.webp";  
-
-  console.log(changeImg.src)
-  console.log(img1)
-  console.log(caminho)
-
+  var img2 = caminho + "ASSETS/erick-light.webp";
 
   if (changeImg.src === img1) {
     changeImg.setAttribute("src", img2);
@@ -19,16 +14,3 @@ button.addEventListener("click", () => {
     changeImg.setAttribute("src", img1);
   }
 });
-
-//TROCA DE IMAGENS
-
-/*function click(){
-  var img1 = 'ASSETS/minha-carinha2.jpg';
-  var img2 = 'ASSETS/hard-skills-svg/photoshop.svg';
-  var element = document.getElementById('mudar-foto');
-  if(element.src === img1){
-    element.src = img2;
-  }else if(element.src === img2){
-    element.src = img1;
-  }
-}*/
--- SCRIPT/valida.js
+++ SCRIPT/valida.js
@@ -4,129 +4,158 @@ let email = document.getElementById("e-mail");
 let repeteEmail = document.getElementById("repete-e-mail");
 let submit = document.getElementById("submit-form");
 
-var erroSubmitNome = true
-var erroSubmitEmail = true
-var erroSubmitRepeteEmail = true
+var erroSubmitNome = true;
+var erroSubmitEmail = true;
+var erroSubmitRepeteEmail = true;
 
 // ==================== VALIDA O NOME ========================
-const messageNome = document.querySelector('#messageNome');
+const messageNome = document.querySelector("#messageNome");
 
-nome.addEventListener('keyup', () => {
-  checkEmail()
+nome.addEventListener("keyup", () => {
+  checkEmail();
   try {
     tamanhoNome = nome.value.length;
-    const onlyLetras  = /^[a-zA-Z \W]*$/;
-      if (!onlyLetras.test(nome.value)) {
-          document.getElementById('messageNome').setAttribute('style', 'display:block');
-          erroSubmitNome = true
-          throw {nome_error: 'Apenas letras'};
-      }
-      
-      if (tamanhoNome < 10) {
-        document.getElementById('messageNome').setAttribute('style', 'display:block');
-        erroSubmitNome = true
-        throw {nome_error: 'Digite seu nome completo'};        
-      } 
+    const onlyLetras = /^[a-zA-Z \W]*$/;
+    if (!onlyLetras.test(nome.value)) {
+      document
+        .getElementById("messageNome")
+        .setAttribute("style", "display:block");
+      erroSubmitNome = true;
+      throw { nome_error: "Apenas letras" };
+    }
 
-      if (onlyLetras.test(email.value) == true) {
-        erroSubmitNome = false
-        document.getElementById('messageNome').setAttribute('style', 'display:none');
-      }
+    if (tamanhoNome < 10) {
+      document
+        .getElementById("messageNome")
+        .setAttribute("style", "display:block");
+      erroSubmitNome = true;
+      throw { nome_error: "Digite seu nome completo" };
+    }
 
+    if (onlyLetras.test(email.value) == true) {
+      erroSubmitNome = false;
+      document
+        .getElementById("messageNome")
+        .setAttribute("style", "display:none");
+    }
   } catch (error) {
-    if(error?.nome_error){
+    if (error?.nome_error) {
       messageNome.textContent = error.nome_error;
     }
-    }
-  })
+  }
+});
 
 // ==================== VALIDA O EMAIL ========================
-const messageEmail = document.querySelector('#messageEmail');
+const messageEmail = document.querySelector("#messageEmail");
 
-email.addEventListener('keyup',  () => {
-  checkEmail()
+email.addEventListener("keyup", () => {
+  checkEmail();
   try {
-    const emailValido  = /\S+@\S+\.\S+/;
-      if (!emailValido.test(email.value)) {
-          document.getElementById('messageEmail').setAttribute('style', 'display:block');
-          erroSubmitEmail = true
-          throw {email_error: 'Digite um e-mail válido'};
-      } 
-
-      if (emailValido.test(email.value) == true) {
-        erroSubmitEmail = false
-        document.getElementById('messageEmail').setAttribute('style', 'display: none');
-      }
-      
-  } catch (error) {
-      if(error?.email_error){
-        messageEmail.textContent = error.email_error;
-      } 
+    const emailValido = /\S+@\S+\.\S+/;
+    if (!emailValido.test(email.value)) {
+      document
+        .getElementById("messageEmail")
+        .setAttribute("style", "display:block");
+      erroSubmitEmail = true;
+      throw { email_error: "Digite um e-mail válido" };
     }
-  })
 
-  // ==================== CONFERE O EMAIL ========================
+    if (emailValido.test(email.value) == true) {
+      erroSubmitEmail = false;
+      document
+        .getElementById("messageEmail")
+        .setAttribute("style", "display: none");
+    }
+  } catch (error) {
+    if (error?.email_error) {
+      messageEmail.textContent = error.email_error;
+    }
+  }
+});
 
-  repeteEmail.addEventListener('keyup', () => {
-      checkEmail()
-      try {
-        if (email.value != repeteEmail.value || repeteEmail.value.length == 0 || erroSubmitEmail == true) {
-          document.getElementById('messageRepeteEmail').setAttribute('style', 'display:block');
-          erroSubmitRepeteEmail = true
-          throw {repeteEmail_error: 'Os e-mails não conferem.'};
-        }
-        
-        if (email.value === repeteEmail.value) {
-          erroSubmitRepeteEmail = false
-          document.getElementById('messageRepeteEmail').setAttribute('style', 'display: none');
-        }
-      } catch (error) {
-          if(error?.repeteEmail_error){
-            messageRepeteEmail.textContent = error.repeteEmail_error;
-          } 
-      }
-  })
+// ==================== CONFERE O EMAIL ========================
 
+repeteEmail.addEventListener("keyup", () => {
+  checkEmail();
+  try {
+    if (
+      email.value != repeteEmail.value ||
+      repeteEmail.value.length == 0 ||
+      erroSubmitEmail == true
+    ) {
+      document
+        .getElementById("messageRepeteEmail")
+        .setAttribute("style", "display:block");
+      erroSubmitRepeteEmail = true;
+      throw { repeteEmail_error: "Os e-mails não conferem." };
+    }
 
-
-
+    if (email.value === repeteEmail.value) {
+      erroSubmitRepeteEmail = false;
+      document
+        .getElementById("messageRepeteEmail")
+        .setAttribute("style", "display: none");
+    }
+  } catch (error) {
+    if (error?.repeteEmail_error) {
+      messageRepeteEmail.textContent = error.repeteEmail_error;
+    }
+  }
+});
 
 // ===================== CHECA O SUBMIT ========================
-const messageSubmit = document.querySelector('#messageSubmit');
+const messageSubmit = document.querySelector("#messageSubmit");
 
-submit.addEventListener("click", function(event) {
-  
-  if (erroSubmitNome == true || erroSubmitEmail == true || erroSubmitRepeteEmail == true) {
-    document.getElementById('messageSubmit').setAttribute('style', 'display: block');
-    document.getElementById('messageSubmit').innerHTML = "Preencha todos os campos corretamente";
+submit.addEventListener("click", function (event) {
+  if (
+    erroSubmitNome == true ||
+    erroSubmitEmail == true ||
+    erroSubmitRepeteEmail == true
+  ) {
+    document
+      .getElementById("messageSubmit")
+      .setAttribute("style", "display: block");
+    document.getElementById("messageSubmit").innerHTML =
+      "Preencha todos os campos corretamente";
     event.preventDefault();
-  }else if (erroSubmitNome == false && erroSubmitEmail == false && erroSubmitRepeteEmail == false) {
-    document.getElementById('messageSubmit').setAttribute('style', 'display: none');
+  } else if (
+    erroSubmitNome == false &&
+    erroSubmitEmail == false &&
+    erroSubmitRepeteEmail == false
+  ) {
+    document
+      .getElementById("messageSubmit")
+      .setAttribute("style", "display: none");
   }
-})
+});
 
 function checkEmail() {
-  email.addEventListener('keyup', () => {
+  email.addEventListener("keyup", () => {
     if (email.value != repeteEmail.value) {
-      document.getElementById('messageRepeteEmail').setAttribute('style', 'display:block');
-      erroSubmitRepeteEmail = true
+      document
+        .getElementById("messageRepeteEmail")
+        .setAttribute("style", "display:block");
+      erroSubmitRepeteEmail = true;
       messageRepeteEmail.textContent = "Os e-mails não conferem.";
     }
-  })
+  });
 
-  repeteEmail.addEventListener('focusin', () => {
+  repeteEmail.addEventListener("focusin", () => {
     if (email.value != repeteEmail.value) {
-      document.getElementById('messageRepeteEmail').setAttribute('style', 'display:block');
-      erroSubmitRepeteEmail = true
+      document
+        .getElementById("messageRepeteEmail")
+        .setAttribute("style", "display:block");
+      erroSubmitRepeteEmail = true;
       messageRepeteEmail.textContent = "Os e-mails não conferem.";
     }
-  })
+  });
 }
 
-
-document.getElementById('repete-e-mail').addEventListener('paste', function(E){
-      E.preventDefault();
-      return false;
+document.getElementById("repete-e-mail").addEventListener(
+  "paste",
+  function (E) {
+    E.preventDefault();
+    return false;
   },
   true
 );
